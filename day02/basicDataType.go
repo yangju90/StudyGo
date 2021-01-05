104acc0c75fe7907ea645c3402b277c65981f59e
yangju90 <yangju90@163.com>
2021-01-06 00:31:18 +0800

    chage

--- day02/basicDataType.go
+++ day02/basicDataType.go
@@ -8,46 +8,43 @@ import (
 var (
 	var32int int32 = 1
 	var64int int64 = 1
-	a int = 10
-	b int = 077
-	c int = 0xff
+	a        int   = 10
+	b        int   = 077
+	c        int   = 0xff
 
 	d bool = true
 
 	varComplex complex64 = 1 + 2i
 
-	s string ="张三"
+	s string = "张三"
 
 	ss string = `张三
 				李四`
 )
 
-
-func main(){
+func main() {
 	pi := math.Pi
-
 
 	fmt.Println(pi)
 
-	fmt.Printf("%f \n" , pi) 
-	fmt.Printf("%.2f \n" , pi) 
+	fmt.Printf("%f \n", pi)
+	fmt.Printf("%.2f \n", pi)
 
 	fmt.Println(varComplex)
 	fmt.Println(s)
-	fmt.Println("字符串长度",len(s))
+	fmt.Println("字符串长度", len(s))
 
 	fmt.Println(ss)
-	fmt.Println("字符串长度",len(ss))
+	fmt.Println("字符串长度", len(ss))
 
-	fmt.Printf("%d \n" , a) // 10进制输出
-	fmt.Printf("%d \n" , b)
-	fmt.Printf("%d \n" , c)
-	fmt.Printf("%b \n" , a) // 2进制输出
-	fmt.Printf("%o \n" , b) // 8进制输出
-	fmt.Printf("%x \n" , c) // 16进制输出
+	fmt.Printf("%d \n", a) // 10进制输出
+	fmt.Printf("%d \n", b)
+	fmt.Printf("%d \n", c)
+	fmt.Printf("%b \n", a) // 2进制输出
+	fmt.Printf("%o \n", b) // 8进制输出
+	fmt.Printf("%x \n", c) // 16进制输出
 
-
-	fmt.Printf("%t \n" , d) 
+	fmt.Printf("%t \n", d)
 
 	traverseString()
 	changeString()
@@ -61,15 +58,15 @@ func main(){
 func traverseString() {
 	s := "hello沙河"
 	for i := 0; i < len(s); i++ {
-		fmt.Printf("%v(%c) \n" , s[i], s[i])
+		fmt.Printf("%v(%c) \n", s[i], s[i])
 	}
-	// 104(h) 101(e) 108(l) 108(l) 111(o) 230(æ) 178(²) 153() 230(æ) 178(²) 179(³) 
+	// 104(h) 101(e) 108(l) 108(l) 111(o) 230(æ) 178(²) 153() 230(æ) 178(²) 179(³)
 
 	fmt.Println()
 
 	for index, r := range s {
-		fmt.Printf("%d :" , index)
-		fmt.Printf("%v(%c) \n" , r, r)
+		fmt.Printf("%d :", index)
+		fmt.Printf("%v(%c) \n", r, r)
 	}
 	// 104(h) 101(e) 108(l) 108(l) 111(o) 27801(沙) 27827(河)
 

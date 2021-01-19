ddc641ef5e9301ff969f7f2c83870c1cfa066072
yangju90 <yangju90@163.com>
2021-01-19 23:51:49 +0800

    commit

--- day05/uintptrDemo.go
+++ day05/uintptrDemo.go
@@ -6,4 +6,21 @@ import (
 
 func Day05() {
 	fmt.Println("=============day05===========")
+	ptrDemo()
+}
+
+func ptrDemo() {
+	a := 10
+	moditfyV(a)
+	fmt.Println(a)
+	moditfyPtrV(&a)
+	fmt.Println(a)
+}
+
+func moditfyV(x int) {
+	x = 100
+}
+
+func moditfyPtrV(x *int) {
+	*x = 1000
 }
